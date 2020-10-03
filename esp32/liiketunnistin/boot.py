1868b77e6588ab940b81f3e74e5ec6518003fd1c
Jari Hiltunen <65418152+divergentti@users.noreply.github.com>
2020-10-03 12:31:55 +0300

    Update boot.py

--- esp32/liiketunnistin/boot.py
+++ esp32/liiketunnistin/boot.py
@@ -1,31 +1,40 @@
+""" Boottiversio 1.0
+
+Parametrit tuodaan parametrit.py-tidesosta. Vähintään tarvitaan SSID1 ja SALASANA1 joilla kerrotaan
+mihin Wifi-AP halutaan yhdistää. Mikäli WebREPL:ä halutaan käyttää, tulee ensimmäisellä kerralla
+käynnistää komento import webrepl_setup, joka luo tiedoston webrepl_cfg.py laitteen juureen.
+
+Komennoilla import os, os.rename('vanha_tiedosto', 'uusi_tiedosto') tai os.remove('tiedostonimi')
+voit käsitellä laitteen tiedostoja joko WebREPL tai konsoliportin kautta.
+
+3.10.2020 Jari Hiltunen """
+
+
 import utime
 import time
 import machine
 import network
 import ntptime
 import esp
-import webrepl  # webbihallinta - asenna komennolla import webrepl_setup
+import webrepl
 from time import sleep
-# Parametrit tuodaan parametrit.py-tiedostosta
-from parametrit import SSID1, SSID2, SALASANA1, SALASANA2, WEBREPL_SALASANA, NTPPALVELIN
+from parametrit import SSID1, SSID2, SALASANA1, SALASANA2, WEBREPL_SALASANA, NTPPALVELIN, DHCP_NIMI
 
 machine.freq(240000000)  # Aluksi maksimipotku prosessoriin
 esp.osdebug(None)
-webrepl.start(password=WEBREPL_SALASANA)
 wificlient_if = network.WLAN(network.STA_IF)
 wificlient_if.active(False)
-ntptime.host = NTPPALVELIN
 
 
 def yhdista_wifi(ssid_nimi, salasana):
     global wificlient_if
     print("Kokeillaan %s" % ssid_nimi)
     wificlient_if.active(True)
+    if DHCP_NIMI is not None:
+        wificlient_if.config(dhcp_hostname=DHCP_NIMI)
     wificlient_if.connect(ssid_nimi, salasana)
     time.sleep(2)
     if wificlient_if.isconnected():
-        print('Verkon kokoonpano:', wificlient_if.ifconfig())
-        print("Signaalitaso %s" % (wificlient_if.status('rssi')))
         aseta_aika()
         return True
     else:
@@ -38,27 +47,52 @@ def ei_voida_yhdistaa():
     machine.reset()
 
 
-
 def aseta_aika():
     try:
+        if NTPPALVELIN is not None:
+            ntptime.host = NTPPALVELIN
         ntptime.settime()
-        print(utime.localtime(utime.time()))
     except OSError as e:
         print("NTP-palvelimelta %s ei saatu aikaa! Virhe %s" % (NTPPALVELIN, e))
         ei_voida_yhdistaa()
-    try:
-        webrepl.start()  # kaynnistetaan WebREPL
-        machine.freq(80000000)  # hidastetaan
-    except OSError as e:
-        print("WebREPL ei kaynnisty. Virhe %s" % e)
-        ei_voida_yhdistaa()
 
 
-try:
-    yhdista_wifi(SSID1, SALASANA1)
-except False:
-    try:
-        yhdista_wifi(SSID2, SALASANA2)
-    except False:
-        print("Yhdistys ei onnistu, bootataan!")
-        ei_voida_yhdistaa()
+def jatka():
+    """
+    :rtype: wificlient_if
+    """
+    if WEBREPL_SALASANA is not None:
+        try:
+            webrepl.start(password=WEBREPL_SALASANA)
+            webrepl.start()
+        except OSError as e:
+            print("WebREPL ei kaynnisty. Virhe %s" % e)
+            ei_voida_yhdistaa()
+    print("Aika: %s " % utime.localtime(utime.time()))
+    print('Verkon kokoonpano:', wificlient_if.ifconfig())
+    print("WiFi signaalitaso %s" % (wificlient_if.status('rssi')))
+    machine.freq(80000000)  # hidastetaan
+
+
+if wificlient_if.isconnected() is True:
+    jatka()
+
+
+if SSID1 is None and SSID2 is None:
+    print("Aseta SSID1 ja/tai SSID2 nimi ja salasana paramterit.py-tiedostossa!")
+    raise Exception("Aseta SSID1 ja/tai SSID2 nimi ja salasana paramterit.py-tiedostossa!")
+
+
+if wificlient_if.isconnected() is False:
+    if SSID1 is not None and SALASANA1 is not None:
+        try:
+            yhdista_wifi(SSID1, SALASANA1)
+        except False:
+            if SSID2 is not None and SALASANA2 is not None:
+                try:
+                    yhdista_wifi(SSID2, SALASANA2)
+                except False:
+                    print("Yhdistys ei onnistu, bootataan!")
+                    ei_voida_yhdistaa()
+else:
+    jatka()
